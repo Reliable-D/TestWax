b9d4d4b42e9b54f61d2d40e40df6d115fa455e11
贝塔(邓乐) <le.deng@fanli.com>
2018-04-16 16:29:48 +0800

    init

---
+++ TestWax/TestWax/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWax
+//
+//  Created by 邓乐 on 2018/4/16.
+//  Copyright © 2018年 fanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
